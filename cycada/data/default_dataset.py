f534f91364709f0c30be0f206cdf2d2a7c4307a2
Hannes Liik <hannes_liik@hotmail.com>
2019-02-05 17:17:38 +0200

    Add setup.py

--- cycada/data/default_dataset.py
+++ cycada/data/default_dataset.py
@@ -1,6 +1,7 @@
 import os.path
+from glob import glob
+import json
 
-from .cityscapes import remap_labels_to_train_ids
 import numpy as np
 import scipy.io
 import torch
@@ -11,9 +12,14 @@ from .data_loader import register_data_params, register_dataset_obj
 from .data_loader import DatasetParams
 from .cityscapes import id2label as LABEL2TRAIN
 
+def colors2labels(arr, label2color, ignore_label=255):
+    out = ignore_label * np.ones(arr.shape, dtype=np.uint8)
+    for label, color in label2color.items():
+        out[arr == id] = int(label)
+    return out
 
 @register_data_params('default')
-class GTA5Params(DatasetParams):
+class DefaultParams(DatasetParams):
     num_channels = 3
     image_size = 1024
     mean = 0.5
@@ -24,54 +30,72 @@ class GTA5Params(DatasetParams):
 
 @register_dataset_obj('default')
 class DefaultDataset(data.Dataset):
+    """
+    Dataset Structure:
+    /../data_dir/
+        /some_other_dataset/
+        /dataset_name/  (root)
+            remap.json
+            /train/
+                /images/
+                    *.png
+                /labels/
+                    *.png
+            /test/
+                /images/
+                    *.png
+                /labels/
+                    *.png
+            /val/
+                /images/
+                    *.png
+                /labels/
+                    *.png
+    """
 
     def __init__(self, root, num_cls=19, split='train', remap_labels=True,
                  transform=None, target_transform=None):
         self.root = root
         self.split = split
+        self.data_dir = os.path.join(self.root, self.split)
+        assert os.path.exists(self.data_dir), f"{self.data_dir} does not exist"
+
         self.remap_labels = remap_labels
-        self.ids = self.collect_ids()
+        self.img_ids, self.label_ids = self.collect_ids()
         self.transform = transform
         self.target_transform = target_transform
-        m = scipy.io.loadmat(os.path.join(self.root, 'mapping.mat'))
-        full_classes = [x[0] for x in m['classes'][0]]
-        self.classes = []
-        for old_id, new_id in LABEL2TRAIN.items():
-            if not new_id == 255 and old_id > 0:
-                self.classes.append(full_classes[old_id])
-        self.num_cls = 19
+
+        if remap_labels:
+            l2c_path = os.path.join(self.data_dir, "label2color.json")
+            assert os.path.exists(l2c_path), f"Label to color mapping does not exist at {l2c_path}"
+            with open(l2c_path, "r") as fp:
+                self.label2color = json.load(fp)
 
     def collect_ids(self):
         # Iterate through all the images and remember their paths
-        splits = scipy.io.loadmat(os.path.join(self.root, 'split.mat'))
-        ids = splits['{}Ids'.format(self.split)].squeeze()
-        # only collect ids for which images actually exist
-        ids_actual = []
-        for id in ids:
-            pth = self.img_path(id)
-            if os.path.exists(pth):
-                ids_actual.append(id)
-        return ids_actual
+        # It is presumed, that in the labels directory, if it exists, the images have the same ordering
+
+        img_ids = glob(self.data_dir + "/images/*.jpg") + glob(self.data_dir + "/images/*.png")
+        label_ids = glob(self.data_dir + "/labels/*.jpg") + glob(self.data_dir + "/labels/*.png")
+        print(f"{len(img_ids)} images and {len(label_ids)} labels")
+        return img_ids, label_ids
 
     def img_path(self, id):
-        filename = '{:05d}.png'.format(id)
-        return os.path.join(self.root, 'images', filename)
+        return self.img_ids[id]
 
     def label_path(self, id):
-        filename = '{:05d}.png'.format(id)
-        return os.path.join(self.root, 'labels', filename)
+        return self.label_ids[id]
 
     def __getitem__(self, index):
-        id = self.ids[index]
-        img_path = self.img_path(id)
-        label_path = self.label_path(id)
+        img_path = self.img_path(index)
+        label_path = self.label_path(index)
         img = Image.open(img_path).convert('RGB')
         if self.transform is not None:
             img = self.transform(img)
         target = Image.open(label_path)
         if self.remap_labels:
             target = np.asarray(target)
-            target = remap_labels_to_train_ids(target)
+            target = colors2labels(target, self.label2color)
             target = Image.fromarray(target, 'L')
         if self.target_transform is not None:
             target = self.target_transform(target)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cycada',
+    version='',
+    packages=['cycada', 'cycada.data', 'cycada.tools', 'cycada.models'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
